00bab1cb7e658c78af08ce747c4629848e6a513e
Pritam <pritamattwork@gmail.com>
2025-03-27 09:57:08 +0530

    feat: database setup with neon and drizzle

---
+++ db/schema.ts
@@ -0,0 +1,23 @@
+import {
+  pgTable,
+  text,
+  timestamp,
+  uniqueIndex,
+  uuid,
+} from "drizzle-orm/pg-core";
+
+export const usersTable = pgTable(
+  "users",
+  {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerk_id: text("clerk_id").notNull().unique(),
+    name: text("name").notNull(),
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  },
+  (table) => [uniqueIndex("clerk_id_idx").on(table.clerk_id)]
+);
+
+export type UserInsertType = typeof usersTable.$inferInsert;
+export type UserSelectType = typeof usersTable.$inferSelect;
