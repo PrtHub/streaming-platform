59382e12939a8c7221cf2c01f0f3d5f51ed5e805
Pritam Personal <pritamattwork@gmail.com>
2025-05-19 11:40:48 +0530

    update

--- modules/home/ui/views/trending-view.tsx
+++ modules/home/ui/views/trending-view.tsx
@@ -4,7 +4,7 @@ const TrendingView = () => {
   return (
     <div className="max-w-[2400px] mx-auto mb-10 px-4 flex flex-col  gap-y-6">
       <header className="flex flex-col gap-y-2">
-        <h1 className="text-2xl font-semibold">Trending videos</h1>
+        <h1 className="text-2xl font-semibold">Trending</h1>
         <p className="text-muted-foreground text-xs">
           Most popular videos at the moment
         </p>
